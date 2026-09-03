169999592f64bb0a01a515b88a32f489c31397fb
ken303867-png <ken303867@gmail.com>
2026-09-03 17:08:33 +0900

    feat: add exam sessions IndexedDB store

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,6 +1,7 @@
 import Dexie, { type EntityTable } from 'dexie';
 import type {
   AppMeta,
+  ExamSession,
   LearningHistory,
   Material,
   MaterialHistory,
@@ -20,6 +21,7 @@ export class StudyDatabase extends Dexie {
   mediaBlobs!: EntityTable<MediaBlobRecord, 'media_id'>;
   learningHistory!: EntityTable<LearningHistory, 'questionId'>;
   materialHistory!: EntityTable<MaterialHistory, 'materialId'>;
+  examSessions!: EntityTable<ExamSession, 'id'>;
   meta!: EntityTable<AppMeta, 'key'>;
 
   constructor() {
@@ -42,6 +44,20 @@ export class StudyDatabase extends Dexie {
       mediaBlobs: 'media_id',
       learningHistory: 'questionId, lastResult, lastAnsweredAt, favorite',
       materialHistory: 'materialId, lastViewedAt, favorite, viewed',
+      meta: 'key'
+    });
+
+    this.version(3).stores({
+      questions: 'id, subject, unit, sourceType, questionFormat, importance, revision, *tags',
+      materials: 'id, subject, unit, importance, revision, *tags',
+      sources: 'source_id, source_group, answer_authority',
+      sourceOccurrences:
+        'source_occurrence_id, canonical_question_id, source_id, source_set_id, source_occurrence_order',
+      media: 'media_id, canonical_question_id, media_type, placement_after, display_order, source_id',
+      mediaBlobs: 'media_id',
+      learningHistory: 'questionId, lastResult, lastAnsweredAt, favorite',
+      materialHistory: 'materialId, lastViewedAt, favorite, viewed',
+      examSessions: 'id, startedAt, completedAt, completionReason',
       meta: 'key'
     });
   }
