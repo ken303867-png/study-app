e75ca5aeb0e7d28fa1f58712fa15ac2bc6a69dfe
ken303867-png <ken303867@gmail.com>
2026-09-07 15:28:30 +0900

    fix: hide admin navigation for public users

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { PublicDatasetGate } from './components/PublicDatasetGate';
 import { PwaControls } from './components/PwaControls';
+import { shouldShowAdminTools } from './utils/adminMode';
 import './styles.css';
 import './questionPromptFormatting.css';
 import './practiceSets.css';
@@ -10,6 +11,11 @@ import './clozeSelfAssessment.css';
 import './pwa.css';
 import './progressiveRendering.css';
 import './publicDatasetSync.css';
+import './adminMode.css';
+
+document.documentElement.dataset.adminTools = shouldShowAdminTools(window.location)
+  ? 'visible'
+  : 'hidden';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
